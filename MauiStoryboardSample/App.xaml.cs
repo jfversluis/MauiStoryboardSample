0ecf04d57f4a20ecc430e526176d399f74ede7b5
Gerald Versluis <gerald@verslu.is>
2023-10-19 15:47:02 +0200

    Initial commit

---
+++ MauiStoryboardSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiStoryboardSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiStoryboardSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiStoryboardSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
